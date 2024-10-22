e933b2c41d05a3b4c3d8dcb27fde7b173316a312
UsoltsevI <usoltsev.ia@phystech.edu>
2024-10-22 17:07:19 +0300

    fixed errors in docker client commands

--- docker/client/create.sh
+++ docker/client/create.sh
@@ -3,11 +3,14 @@
 pull_images() {
   echo "Docker pulling images..."
   docker compose pull
+  echo "Pulling is finished"
 }
 
 build_collectora() {
+  BASEDIR=$(dirname "$0")
+  cd $BASEDIR
   echo "Building CollectorA image..."
-  docker compose -f ../../docker-compose.yml build collectora --detach
+  docker compose -f ../../docker-compose.yml build collectora
 }
 
 pull_images
