824c9cf0abbed26d422a255059aed2c45d14fc04
UsoltsevI <usoltsev.ia@phystech.edu>
2024-12-07 19:29:58 +0300

    hbase docker

--- docker/client/create.sh
+++ docker/client/create.sh
@@ -16,9 +16,6 @@ build_collectora() {
 build_hbase() {
     BASEDIR=$(dirname "$0")
     cd $BASEDIR
-    echo "Downloading the necessary archives for the HBase..."
-    wget -P ../hbase/ https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-    wget -P ../hbase/ https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
     echo "Building HBase image..."
     docker compose -f ../../docker-compose.yml build hbase
 }
